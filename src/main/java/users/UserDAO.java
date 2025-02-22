0880e24738dd945548318858a56accbd9af80d8c
jyhwang <stratojy@gmail.com>
2025-02-22 11:52:17 +0900

    feat. DB 접속 및 로그인 처리
    1. maria DB 접속을 위한 커넥션 jar 를 추가
    2. 로그인 처리 구현
    3. 적절한 오류메시지 표현

---
+++ src/main/java/users/UserDAO.java
@@ -0,0 +1,58 @@
+package users;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+  private Connection conn;
+
+  private PreparedStatement pstmt;
+
+  private ResultSet rs;
+
+  public UserDAO() {
+    try {
+      String dbUrl = System.getProperty("db.url");
+      String dbUsername = System.getProperty("db.username");
+      String dbPassword = System.getProperty("db.password");
+      System.out.println(dbUsername + dbPassword);
+      Class.forName("org.mariadb.jdbc.Driver");
+
+      conn = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+
+    } catch (Exception e) {
+      e.getMessage();
+    }
+  }
+
+  public int login(String userId, String userPassword) {
+
+    System.out.println("userId : " + userId);
+    System.out.println("userPassword : " + userPassword);
+
+    String sqlQuery = " SELECT user_password AS userPassword FROM users WHERE user_id = ? ";
+
+    try {
+      pstmt = conn.prepareStatement(sqlQuery);
+      pstmt.setString(1, userId);
+      rs = pstmt.executeQuery();
+
+      if (rs.next()) {
+        if (rs.getString(1).equals(userPassword)) {
+          return 1; // login ok
+        } else {
+          return 0; // 비밀번호 오류
+        }
+      }
+      return -1; // 아이디가 없음.
+    } catch (Exception e) {
+      e.getMessage();
+    }
+    return -2;// 데이터베이스 오류
+  }
+
+}
